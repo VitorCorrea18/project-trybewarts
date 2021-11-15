02e121262d8198b57c8ea0a38ef5d9556a472ad1
Vitor Correa <vitorcorrea18@gmail.com>
2021-11-14 20:00:10 -0400

    Requisito 20 completo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btnEntrar = document.getElementById('btn-entrar'); // guarda o botão btn-entrar nesta variável.
 const agreementBox = document.getElementById('agreement'); // guarda o checkBox agreement nesta variável.
 const btnSend = document.getElementById('submit-btn'); // guarda o botão 'submit-btn' nesta variavel.
+const textArea = document.querySelector('textarea'); // guarda o input de texto nesta variável.
+const maxLength = textArea.getAttribute('maxlength'); // guarda o valor numerico do atributo maxlength da textarea.
+const counter = document.getElementById('counter'); // Elemento span que guarda o numero atual de carecteres do textarea (length).
 
 function checkLogin() {
   // Verifica se o email e a senha correspondem ao esperado.
@@ -33,6 +36,16 @@ window.onload = () => { // Se ativa no momento que a página é carregada.
     checkAgreement(); // chama a função que verifica se o checkBox está marcado.
   });
 };
+
+textArea.addEventListener('keyup', () => { // Se ativa ao digitar na text area.
+  if (textArea.value.length > 0) { // compara se o numero de caracteres na textarea é maior que 0.
+    counter.innerText = `${textArea.value.length} / ${maxLength}`; // atualiza o counter.
+    counter.style.backgroundColor = 'gray';
+  } else {
+    counter.innerText = ''; // zera o counter.
+    counter.style.backgroundColor = 'white';
+  }
+});
 
 btnSend.addEventListener('click', () => {
   // Completar no requisito 21.
